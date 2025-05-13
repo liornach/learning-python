4969ceacc19aea1d39e85073830d646c2b168f14
Nachmias Lior <lias1225@elta.co.il>
2025-05-13 16:15:22 +0300

    first commit

---
+++ sorts/quick_sort.py
@@ -0,0 +1,13 @@
+def sort(list):
+    sort_rec(list, 0, list.len() - 1)
+
+def sort_rec(list):
+    pivot = partition
+    return
+
+def partition(list, lo, hi):
+    
+    return
+
+def pick_pivot(list, lo, hi):
+    return lo
